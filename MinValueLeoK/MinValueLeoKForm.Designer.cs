fa1b3a2590674a7faf54f58840d033799aead713
leokay1515 <36515738+leokay1515@users.noreply.github.com>
2018-05-08 14:01:01 -0400

    Initial commit

---
+++ MinValueLeoK/MinValueLeoKForm.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace MinValueLeoK
+{
+    partial class frmMinValueLeoK
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lstNumbers = new System.Windows.Forms.ListBox();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(96, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(85, 33);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lstNumbers
+            // 
+            this.lstNumbers.FormattingEnabled = true;
+            this.lstNumbers.Location = new System.Drawing.Point(77, 70);
+            this.lstNumbers.Name = "lstNumbers";
+            this.lstNumbers.Size = new System.Drawing.Size(120, 160);
+            this.lstNumbers.TabIndex = 1;
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnswer.Location = new System.Drawing.Point(12, 258);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(0, 20);
+            this.lblAnswer.TabIndex = 2;
+            // 
+            // frmMinValueLeoK
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 302);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.lstNumbers);
+            this.Controls.Add(this.btnStart);
+            this.Name = "frmMinValueLeoK";
+            this.Text = "Minimum Value by Leo Kay";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ListBox lstNumbers;
+        private System.Windows.Forms.Label lblAnswer;
+    }
+}
+
---
+++ MinValueLeoK/MinValueLeoKForm.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MinValueLeoK
+{
+    public partial class frmMinValueLeoK : Form
+    {
+        public frmMinValueLeoK()
+        {
+            InitializeComponent();
+        }
+
+        private int GetMinValue(int[] arrayOfNumbers)
+        {
+            //variables
+            int tmpMinValue = 101;
+
+            //loop through the  array and find the min value
+            foreach (int tmpNumber in arrayOfNumbers)
+            {
+                if (tmpNumber < tmpMinValue)
+                {
+                    tmpMinValue = tmpNumber;
+                }
+            }
+
+            return tmpMinValue;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //variables
+            const int MAX_ARRAY_SIZE = 10;
+            const int MIN_VALUE = -100;
+            const int MAX_VALUE = 100;
+            int[] arrayOfIntegers = new int[MAX_ARRAY_SIZE];
+            int counter, randomNum, minValue;
+            Random randomNumberGenerator = new Random();
+
+            //clear the listbox items
+            this.lstNumbers.Items.Clear();
+
+            //generate the random numbers and assign them to different positions in the array
+            for (counter = 0; counter < MAX_ARRAY_SIZE; counter++)
+            {
+                //generate a random number
+                randomNum = randomNumberGenerator.Next(MIN_VALUE, MAX_VALUE);
+
+                //assign the number to the array
+                arrayOfIntegers[counter] = randomNum;
+
+                //add the number to the listbox
+                this.lstNumbers.Items.Add(randomNum);
+
+                //refresh the form
+                this.Refresh();
+            }
+
+            //get the min value by calling the GetMinValue funtion
+            minValue = GetMinValue(arrayOfIntegers);
+
+            //display the min value
+            this.lblAnswer.Text = "The min value is " + minValue;
+        }
+    }
+}
